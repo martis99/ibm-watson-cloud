c2db1c27f982ca87ff1d3e24d609d9bb358f48e6
martis99 <bmartynas99@gmail.com>
2022-03-02 13:55:30 +0200

    Make one section

--- luci-app-ibm-watson-cloud/luasrc/model/cbi/ibm-watson-cloud_model.lua
+++ luci-app-ibm-watson-cloud/luasrc/model/cbi/ibm-watson-cloud_model.lua
@@ -1,14 +1,12 @@
-map = Map("ibm-watson-cloud", "IBM Watson Cloud")
+map = Map("ibm-watson-cloud")
 
-main_sct = map:section(NamedSection, "main_sct", "ibm-watson-cloud")
+main_sct = map:section(NamedSection, "main_sct", "ibm-watson-cloud", "IBM Watson Cloud")
 enable = main_sct:option(Flag, "enable", "Enable", "Enable program")
 interval = main_sct:option(Value, "interval", "Interval", "Data update interval")
 interval.datatype = "uinteger"
-
-device_sct = map:section(NamedSection, "device_sct", "ibm-watson-cloud", "Device Configuration")
-org = device_sct:option(Value, "org", "Organization")
-type = device_sct:option(Value, "type", "Device type")
-device = device_sct:option(Value, "device", "Device")
-auth = device_sct:option(Value, "auth", "Authorization token");
+org = main_sct:option(Value, "org", "Organization")
+type = main_sct:option(Value, "type", "Device type")
+device = main_sct:option(Value, "device", "Device")
+auth = main_sct:option(Value, "auth", "Authorization token");
 
 return map
